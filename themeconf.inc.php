3905df4af42eb9a1b8feb2e7af5b8823b45cdaa4
edison23 <klementdavid@gmail.com>
2019-10-26 12:19:43 +0200

    Initial commit of the already working theme

---
+++ themeconf.inc.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Theme Name: Bright Future
+Version: 1
+Description: Bright Piwigo theme based on default theme
+Theme URI: http://piwigo.org/ext/extension_view.php?eid=XXX
+Author: edison23
+Author URI: https://edison23.net/photography
+*/
+$themeconf = array(
+  'name'          => 'bright-future',
+  'parent'        => 'default'
+);
+?>
